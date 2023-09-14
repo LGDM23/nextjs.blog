3e23dca11c21b5b39cad69d79e3a529320293989
LGDM23 <144338698+LGDM23@users.noreply.github.com>
2023-09-14 11:39:59 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,19 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p> Lou Guerrig Mendoza My Hobbies are Playing, Powerlifting, my favorite music is Remember the name. my favorite food is
 Pizza.</p>
+     <p>10 Things That Require Zero Talent</p>
+        <ul>
+        <li>1. Being on Time</li>
+        <li>2. Making An Effort</li>
+        <li>3. Being High Energy</li>
+        <li>4. Having A Positive Attitude</li>
+        <li>5. Being Passionate</li>
+        <li>6. Using Good Body Language</li>
+        <li>7. Being Coachable</li>
+        <li>8. Doing A Little Extra</li>
+        <li>9. Being Prepared</li>
+        <li>10. Having A Strong Work Ethic</li>
+        </ul>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
